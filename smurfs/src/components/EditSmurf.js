433c6734765e6ec2c53b00cfe0c1b7fbb8e8aa01
anthonyvigz <anthonyvigz@gmail.com>
2019-07-20 17:24:40 -0400

    successfully added data to edit smurf

--- smurfs/src/components/EditSmurf.js
+++ smurfs/src/components/EditSmurf.js
@@ -7,18 +7,43 @@ class EditSmurf extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      smurfs: [],
       name: '',
       age: '',
       height: ''
     };
   }
 
+  componentDidMount() {
+    const smurf = this.props.smurfs.find(i => String(i.id) === this.props.match.params.id);
+
+    this.setState({
+        name: smurf.name,
+        age: smurf.age,
+        height: smurf.height
+    })
+  }
 
   render() {
+
+      // deconstructing state with smurf properties
+      const { name, age, height } = this.state
+      
       return (
-        <h1>hey</h1>
+        <h1>{name}</h1>
       )
   }
 }
 
-export default EditSmurf;
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default(
+	connect(
+		mapStateToProps,
+		null,
+	)(EditSmurf)
+)
